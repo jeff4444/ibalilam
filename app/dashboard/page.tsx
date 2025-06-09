b01d96a0818065140e2696f63a79368d3bea604d
Jeff Mofor <allojeff444@gmail.com>
2025-06-08 22:21:10 -0400

    Fix center divs

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -120,7 +120,7 @@ export default function DashboardPage() {
         </nav>
       </header>
 
-      <div className="flex-1 space-y-4 p-4 md:p-8 pt-6 flex items-center justify-center">
+      <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
           <div className="flex items-center space-x-2">
